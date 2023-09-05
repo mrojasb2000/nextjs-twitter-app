8ce541dc2d11e41debf1dc213eed3dbab4dd228f
maalroja <maalroja@bancolombia.com.co>
2023-09-05 18:47:39 -0300

    feat: add auth with Github access token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home () {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { AuthButton } from './components/auth-button'
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Welcome Twitter NextJS
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </main>
   )
 }
