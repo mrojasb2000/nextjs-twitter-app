810f662e2bfb9ab2c5048701f478d3df98be42c3
maalroja <maalroja@bancolombia.com.co>
2023-09-06 17:32:40 -0300

    chore: add reset Html Form elements

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,6 +1,7 @@
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { revalidatePath } from 'next/cache'
+'use client'
+
+import { addPost } from '../actions/add-post-action'
+import { useRef } from 'react'
 
 export function ComponentPost({
   userAvatarUrl,
@@ -9,21 +10,17 @@ export function ComponentPost({
   userAvatarUrl: string
   userName: string
 }) {
-  const addPost = async (formData: FormData) => {
-    'use server'
-    const content = formData.get('content')
-    if (content === null) return
+  const formRef = useRef<HTMLFormElement>(null)
 
-    const supabase = createServerActionClient({ cookies })
-    const {
-      data: { user },
-    } = await supabase.auth.getUser()
-    if (user === null) return
-    await supabase.from('posts').insert({ content, user_id: user.id })
-    revalidatePath('/')
-  }
   return (
-    <form action={addPost} className="flex flex-1 flex-col gap-y-4">
+    <form
+      ref={formRef}
+      action={async (formData) => {
+        await addPost(formData)
+        formRef.current?.reset()
+      }}
+      className="flex flex-1 flex-col gap-y-4"
+    >
       <div className="flex flex-1 flex-col gap-y-4 p-4 border-b border-white/20">
         <div className="flex flex-row space-x-4">
           <img
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,22 @@ export default async function Home() {
   const supabase = createServerComponentClient<Database>({
     cookies,
   })
-  const { data: posts } = await supabase.from('posts').select('*, users(*)')
   const {
     data: { session },
   } = await supabase.auth.getSession()
-  const userAvatar = session?.user?.user_metadata?.avatar_url
-  const userName = session?.user?.user_metadata?.name
 
   if (session === null) {
     redirect('/login')
   }
+
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, users(*)')
+    .order('created_at', { ascending: false })
+  
+  const userAvatar = session?.user?.user_metadata?.avatar_url
+  const userName = session?.user?.user_metadata?.name
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Welcome Twitter NextJS
