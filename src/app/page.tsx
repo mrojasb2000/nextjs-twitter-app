cea2a49d84d047f133dc16541f85829bdd11cdc8
maalroja <maalroja@bancolombia.com.co>
2023-09-06 10:26:27 -0300

    feat: generate Supabase data types and add specification types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
 import { AuthButtonServer } from './components/auth-button-server'
+import { type Database } from './types/database'
 
 export default async function Home() {
-  const supabase = createServerComponentClient({
+  const supabase = createServerComponentClient<Database>({
     cookies,
   })
   const { data: posts } = await supabase.from('posts').select('*, users(*)')
